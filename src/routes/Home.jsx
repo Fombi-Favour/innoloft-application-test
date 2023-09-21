11d64f42cc7fb014fff40c756dd68013adcb0069
Fombi-Favour <fombifavour@gmail.com>
2023-09-21 20:03:27 +0100

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col justify-center p-2'>
+      <h1 className='font-bold'>Home Page</h1>
+      <NavLink to='product' className='bg-blue-600 rounded-md p-3 text-white'>Go to the product</NavLink>
+    </div>
   )
 }
 
