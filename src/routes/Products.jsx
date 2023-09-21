977aab374ea09b5fd1422dad396224e27a879386
Fombi-Favour <fombifavour@gmail.com>
2023-09-21 20:03:30 +0100

    Update Products.jsx

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className='flex flex-col justify-center p-2 gap-2'>
+      <h1 className='font-bold'>Product Page</h1>
+      <NavLink to='edit' className='bg-blue-600 rounded-md p-3 text-white'>Edit the product</NavLink>
+      <NavLink to='/' className='bg-blue-600 rounded-md p-3 text-white'>Back to home page</NavLink>
+    </div>
   )
 }
 
