36f4b336dda2ff389e29082a48f9f85295bcabb7
Fombi-Favour <fombifavour@gmail.com>
2023-09-21 20:02:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './routes/Home';
 import Products from './routes/Products'
+import Edit from './routes/Edit'
 import Layout from './components/Layout'
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='product' element={<Products />} />
+          <Route path='product/edit' element={<Edit />} />
         </Route>
       </Routes>
     </>
