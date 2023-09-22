d411009f713c4380e6bd05a936cdc187bef486de
Fombi-Favour <fombifavour@gmail.com>
2023-09-22 12:28:11 +0100

    Update Products.jsx

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -10,7 +10,7 @@ import Home from '../assets/inno_home.svg';
 
 const Products = () => {
   return (
-    <main className='border-2 p-5 mx-2'>
+    <main className='py-5 pr-3 md:mx-2 lg:-ml-6'>
       {/* sub nav-bar */}
       <div className='flex flex-col gap-3 md:flex-row md:items-center md:justify-between md:gap-0'>
         <div className='flex items-center gap-1 text-[#6B7280] text-[14px]'>
@@ -22,7 +22,7 @@ const Products = () => {
         </div>
         <NavLink to='edit' className='bg-[#272E71] w-11 text-center rounded-md p-1 text-white'>Edit</NavLink>
       </div>
-      <div className='grid grid-row-1 md:grid-cols-[1fr_auto] gap-2'>
+      <div className='grid grid-row-2 lg:grid-row-1 grid-cols-[auto] lg:grid-cols-[1fr_auto] lg:gap-2'>
         {/* main and user-info sections */}
         <ProductMain />
         <UserInfo />
